77448b973ae806a4a8eae1f93bc8f571a026e073
Diovane Barbieri <diovane.gabriel@gmail.com>
2025-08-22 17:08:39 -0300

    mail with attach

--- src/SES.php
+++ src/SES.php
@@ -35,6 +35,80 @@ class SES extends AWS
         ]);
     }
 
+    public function sendRaw($toAddress, string $subject, string $body, bool $isHtml = true, string $fromUser = null, $attachContent = null, $attachFilename = null)
+    {
+
+        if ($fromUser) {
+            $from = $fromUser . " <" . $this->getFromAddress() . ">";
+        } else {
+            $from = $this->getFromAddress();
+        }
+
+        // Boundaries MIME
+        $boundaryMixed = 'mixed-' . md5((string) microtime(true));
+        $boundaryAlt   = 'alt-'   . md5((string) (microtime(true) + 1));
+
+        $headers  = [];
+        $headers[] = "From: {$from}";
+        $headers[] = "To: {$toAddress}";
+        $headers[] = "Subject: {$subject}";
+        $headers[] = "MIME-Version: 1.0";
+        $headers[] = "Content-Type: multipart/mixed; boundary=\"{$boundaryMixed}\"";
+
+        $mime  = implode("\r\n", $headers) . "\r\n\r\n";
+        $mime .= "--{$boundaryMixed}\r\n";
+        $mime .= "Content-Type: multipart/alternative; boundary=\"{$boundaryAlt}\"\r\n\r\n";
+
+        if ($isHtml) {
+            $mime .= "--{$boundaryAlt}\r\n";
+            $mime .= "Content-Type: text/html; charset=\"UTF-8\"\r\n";
+            $mime .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+            $mime .= "{$body}\r\n\r\n";
+        } else {
+            $mime .= "--{$boundaryAlt}\r\n";
+            $mime .= "Content-Type: text/plain; charset=\"UTF-8\"\r\n";
+            $mime .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+            $mime .= "{$body}\r\n\r\n";
+        }
+
+        // Fecha alternativa
+        $mime .= "--{$boundaryAlt}--\r\n\r\n";
+
+        if ($attachContent) {
+            $base64 = chunk_split(base64_encode($attachContent));
+            $filename = $attachFilename ?: "file";
+            // Anexo
+            $mime .= "--{$boundaryMixed}\r\n";
+            $mime .= "Content-Type: application/pdf; name=\"{$filename}\"\r\n";
+            $mime .= "Content-Description: {$filename}\r\n";
+            $mime .= "Content-Disposition: attachment; filename=\"{$filename}\"; size=" . strlen($attachContent) . ";\r\n";
+            $mime .= "Content-Transfer-Encoding: base64\r\n\r\n";
+            $mime .= "{$base64}\r\n";
+            $mime .= "--{$boundaryMixed}--";
+        }
+
+        try {
+            $result = $this->ses->sendRawEmail([
+                'RawMessage'   => ['Data' => $mime],
+                'Source'       => $from,
+                'Destinations' => [$toAddress], // opcional; o "To:" do cabeçalho já define
+            ]);
+
+            $this->log(json_encode($result));
+
+            return $result->get('MessageId');
+        } catch (SesException $e) {
+            $xmlResponse = $e->getResponse()->getBody()->__toString();
+
+            $this->log($xmlResponse, Graylog::LEVEL_ERROR);
+
+            throw $e;
+        } catch (Error | Exception $e) {
+            $this->log($e->getMessage(), Graylog::LEVEL_FATAL);
+            throw $e;
+        }
+    }
+
     public function send($toAddresses, string $subject, string $body, $replyToAddresses = null, bool $isHtml = true, $fromAddress = null, string $fromUser = null)
     {
         $toAddresses = is_array($toAddresses) ? $toAddresses : [$toAddresses];
